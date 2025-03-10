b6c3edd76688ebb79101ab9cdcdf1159a4043dc7
SanSiro2603 <143419496+SanSiro2603@users.noreply.github.com>
2025-03-10 12:53:37 +0800

    database-model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kelas;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -18,11 +20,32 @@ class UserController extends Controller
     }
 
     public function create(){
+        return view('create_user', [ 'kelas' => Kelas::all(),
+        ]);
+
         return view('create_user');
         }
     
         public function store(Request $request)
+
 {
+    $validatedData = $request->validate([
+        'nama' => 'required|string|max:255',
+        'npm' => 'required|string|max:255',
+        'kelas_id' => 'required|exists:kelas,id',
+    ]);
+
+    $user = UserModel::create($validatedData);
+
+    $user->load('kelas');
+
+    return view('profile', [
+        'nama' => $user->nama,
+        'npm' => $user->npm,
+        'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+    ]);
+
+
     $data = [
         'nama' => $request->input('nama'),
         'kelas' => $request->input('kelas'),
